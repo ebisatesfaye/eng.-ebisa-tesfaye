5207ae5653f07e54458ebeec35907677c93ea299
Eng. ebisa <ebisatesfaye30@gmail.com>
2025-11-09 21:18:50 +0300

    gitingnore

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,5 +1,5 @@
 import { RevealOnScroll } from "../RevealOnScroll";
-import photo from '../../assets/portifolio.JPG'
+import photo from '../../assets/portifolio.jpg'
 
 export const Home = () => {
     return (
