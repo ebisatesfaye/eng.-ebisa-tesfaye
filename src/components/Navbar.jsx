675aaf4e648fd6a1a03ecd7905f9646cf796ed66
ebisatesfaye <ebisatesfaye30@gmail.com>
2026-02-17 11:50:44 +0300

    logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,42 @@
 import { useEffect } from "react"
+import profile from "../assets/profile2.png"
 
-export const Navbar = ({menuOpen, setMenuOpen}) => {
+export const Navbar = ({ menuOpen, setMenuOpen }) => {
 
     useEffect(() => {
-    document.body.style.overflow = menuOpen ? "hidden" : "";
-  }, [menuOpen]);
-    return  (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
-        <div className="max-w-5xl mx-auto px-4">
-            <div className="flex justify-between items-center h-16">
-                <a href="#home" className="font-mono text-xl font-bold text-white">
-                    
-                    eng<span className="text-blue-500">. ebisan</span>
-                </a>
-              <div
-                    className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
-                    onClick={() => setMenuOpen((prev) => !prev)}
-                >
-                    &#9776;
-                </div>
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    }, [menuOpen]);
+    return (
+        <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+            <div className="max-w-5xl mx-auto px-4 ">
+                <div className="flex justify-between items-center h-16">
+                    <a href="#home" className="font-mono text-xl font-bold text-white flex items-center">
 
-                <div className="hidden md:flex items-center space-x-8">
-                    <a href="#home" className="text-gray-300 hove:text-white transition-colors">
-                         Home  </a>
+                        <img src={profile} alt="Profile" className="w-36 h-36 rounded-full pt-3 pb-3 mt-4" />
+                        eng<span className="text-blue-500">. ebisan</span>
+                    </a>
+                    <div
+                        className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
+                        onClick={() => setMenuOpen((prev) => !prev)}
+                    >
+                        &#9776;
+                    </div>
 
-                    <a href="#about" className="text-gray-300 hove:text-white transition-colors">
-                         About  </a>
+                    <div className="hidden md:flex items-center space-x-8">
+                        <a href="#home" className="text-gray-300 hove:text-white transition-colors">
+                            Home  </a>
 
-                    <a href="#projects" className="text-gray-300 hove:text-white transition-colors">
-                         Projects  </a>
+                        <a href="#about" className="text-gray-300 hove:text-white transition-colors">
+                            About  </a>
 
-                    <a href="#contact" className="text-gray-300 hove:text-white transition-colors">
-                         Contact  </a>
+                        <a href="#projects" className="text-gray-300 hove:text-white transition-colors">
+                            Projects  </a>
+
+                        <a href="#contact" className="text-gray-300 hove:text-white transition-colors">
+                            Contact  </a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
+        </nav>
     )
 }
